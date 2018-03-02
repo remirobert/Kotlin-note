66223969b9b29a0f2478d8d1c5e6d4dba8251bbd
r é m i  <remi.robert@epitech.eu>
2018-03-02 11:15:42 +0800

    add base project

---
+++ app/src/main/java/com/kotlinnote/remirobert/kotlinnote/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kotlinnote.remirobert.kotlinnote
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
